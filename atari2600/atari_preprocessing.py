a7b323584deb83aa36146258f9bd864cd347032d
Dick Ameln <amelndjd@gmail.com>
2018-12-12 12:42:16 +0100

    fixed memory issue by casting images to uint8

--- atari2600/atari_preprocessing.py
+++ atari2600/atari_preprocessing.py
@@ -15,5 +15,6 @@ def preprocess(img):
     img = rgb2gray(img)
     img = img[34:34+160, 0:160]
     img = resize(img, (84, 84), mode='constant')
+    img = img.astype(np.uint8)
     return img
     # return to_grayscale(downsample(img))
