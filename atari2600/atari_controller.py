a9ccface75ef06c97ce2b6e2a7b6dde0c29e3d43
Dick Ameln <amelndjd@gmail.com>
2018-12-16 16:41:38 +0100

    iterations noow measured in 1-iterations instead of frames

--- atari2600/atari_controller.py
+++ atari2600/atari_controller.py
@@ -32,7 +32,7 @@ class AtariController:
         state_frame = np.reshape([frame], (1, ATARI_SHAPE[0], ATARI_SHAPE[1], 1))
         self.state = np.append(self.state[:, :, :, 1:], state_frame, axis=3)
         # clip reward
-        reward = np.sign(reward)
+        # reward = np.sign(reward)
 
         return frame, reward, is_done, life_lost
 
