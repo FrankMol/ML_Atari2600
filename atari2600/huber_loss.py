0ac66722da63daa91e49268c90f3d918568b3edf
Dick Ameln <amelndjd@gmail.com>
2018-12-22 15:04:27 +0100

    debjd

--- atari2600/huber_loss.py
+++ atari2600/huber_loss.py
@@ -7,11 +7,11 @@ def huber_loss(y, q_value):
     loss = K.mean(0.5 * K.square(quadratic_part) + linear_part)
     return loss
 
-def huber_loss2(a, b, in_keras=True):
-    error = a - b
-    quadratic_term = error * error / 2
-    linear_term = abs(error) - 1 / 2
-    use_linear_term = (abs(error) > 1.0)
-    if in_keras:
-        use_linear_term = K.cast(use_linear_term, 'float32')
-    return use_linear_term * linear_term + (1 - use_linear_term) * quadratic_term
+# def huber_loss2(a, b, in_keras=True):
+#     error = a - b
+#     quadratic_term = error * error / 2
+#     linear_term = abs(error) - 1 / 2
+#     use_linear_term = (abs(error) > 1.0)
+#     if in_keras:
+#         use_linear_term = K.cast(use_linear_term, 'float32')
+#     return use_linear_term * linear_term + (1 - use_linear_term) * quadratic_term
